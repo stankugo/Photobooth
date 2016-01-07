d5a73ba38ba91dec0d87601de8e59f22e22a14f7
Stephan Kurz <stephan.kurz@mac.com>
2016-01-07 14:42:05 +0100

    debug

--- cam.py
+++ cam.py
@@ -1,14 +1,14 @@
 import time
 import picamera
-import subprocess
+import os
 
 camera = picamera.PiCamera()
 try:
     camera.start_preview()
-    subprocess.call(["~/raspidmx/pngview/./pngview", "-b 0 -l 3 ~/Photobooth/cards/26.png"])
+    os.system("~/raspidmx/pngview/./pngview -b 0 -l 3 ~/Photobooth/cards/26.png")
     
     time.sleep(30)
     camera.stop_preview()
 finally:
     camera.close()
-    subprocess.call(["killall pngview"])
+    os.system("killall pngview")
