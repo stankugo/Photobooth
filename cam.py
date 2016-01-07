e0e3b3c5847426c20bdb7c5ae1b514562279c842
Stephan Kurz <stephan.kurz@mac.com>
2016-01-07 14:49:46 +0100

    debug

--- cam.py
+++ cam.py
@@ -4,6 +4,9 @@ import os
 
 camera = picamera.PiCamera()
 try:
+    camera.resolution = (1067, 800)
+    camera.hflip = True
+    
     camera.start_preview()
     os.system("/home/pi/raspidmx/pngview/./pngview -b 0 -l 3 /home/pi/Photobooth/cards/26.png")
     
