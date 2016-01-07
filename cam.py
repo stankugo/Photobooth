61dcd640ef12ff8d2108b4bd0982bf606523d3cf
Stephan Kurz <stephan.kurz@mac.com>
2016-01-07 14:44:03 +0100

    debug

--- cam.py
+++ cam.py
@@ -5,7 +5,7 @@ import os
 camera = picamera.PiCamera()
 try:
     camera.start_preview()
-    os.system("/home/pi/raspidmx/pngview/./pngview -b 0 -l 3 ~/Photobooth/cards/26.png")
+    os.system("/home/pi/raspidmx/pngview/./pngview -b 0 -l 3 /home/pi/Photobooth/cards/26.png")
     
     time.sleep(30)
     camera.stop_preview()
