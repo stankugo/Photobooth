799a84ffefc9792f5a9507cdf147e69b8fdcce77
Stephan Kurz <stephan.kurz@mac.com>
2016-01-07 14:38:14 +0100

    debug

--- cam.py
+++ cam.py
@@ -4,7 +4,10 @@ import picamera
 camera = picamera.PiCamera()
 try:
     camera.start_preview()
-    time.sleep(10)
+    subprocess.call(["~/raspidmx/pngview/./pngview", "-b 0 -l 3 ~/Photobooth/cards/26.png"])
+    
+    time.sleep(30)
     camera.stop_preview()
 finally:
     camera.close()
+    subprocess.call(["killall pngview"])
