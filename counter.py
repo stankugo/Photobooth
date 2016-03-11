5edf2131124916fde9150e27c06c4ee3f3114627
Stephan Kurz <stephan.kurz@mac.com>
2016-03-11 06:07:35 +0100

    counter

--- counter.py
+++ counter.py
@@ -62,8 +62,8 @@ misc = {
     'port' : '/dev/ttyUSB0'
 }
 
-overlay = None
-counter = None
+overlay = 'none'
+counter = 'none'
 
 camera = picamera.PiCamera()
 camera.resolution = (misc['width'], misc['height'])
@@ -97,7 +97,7 @@ def setup():
     misc['image'] = misc['random']
     
     # KILL EXISTING OVERLAY
-    if overlay != None:
+    if overlay != 'none':
         overlay.terminate()
     overlay = subprocess.Popen(['/home/pi/raspidmx/pngview/./pngview','-b','0','-l','3','/home/pi/Photobooth/cards/' + misc['images'][misc['image']] + '.png'])
 
