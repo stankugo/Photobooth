f3809e07cb3bcf8d17162d2cb9795ceca09a5ec9
Stephan Kurz <stephan.kurz@mac.com>
2016-03-11 02:14:05 +0100

    service

---
+++ service/photobooth.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#/etc/init.d/photobooth.sh
+case "$1" in
+  start)
+    echo "Starting Photobooth"
+    # run application you want to start
+    python /usr/local/bin/python/script.py >/dev/null 2>&1 &
+    ;;
+  stop)
+    echo "Stopping Photobooth"
+    # kill application you want to stop
+    killall python
+    ;;
+  *)
+    echo "Usage: /etc/init.d/photobooth {start|stop}"
+    exit 1
+    ;;
+esac
+exit 0
+
