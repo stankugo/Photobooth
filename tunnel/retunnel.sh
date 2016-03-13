7107c384491cb1dcb06c01b5b8810c14f7bc08e9
Stephan Kurz <stephan.kurz@mac.com>
2016-03-13 18:32:10 +0100

    retunnel

--- tunnel/retunnel.sh
+++ tunnel/retunnel.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+createTunnel() {
+  /usr/bin/ssh -N -R 2400:localhost:22 root@morgen.jetzt
+  if [[ $? -eq 0 ]]; then
+    echo Tunnel to jumpbox created successfully
+  else
+    echo An error occurred creating a tunnel to jumpbox. RC was $?
+  fi
+}
+
+echo Creating new tunnel connection
 kill /bin/pidof ssh
+createTunnel
