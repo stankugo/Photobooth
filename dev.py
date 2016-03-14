b51622442e5d3cc45b57d37a9e67f6b0eb24f9a5
Stephan Kurz <stephan.kurz@mac.com>
2016-03-14 10:26:53 +0100

    dev

--- dev.py
+++ dev.py
@@ -193,9 +193,9 @@ def snapshot(image):
     print 'resize'
     
     # MERGING IMAGES
-	resize_canvas(misc['snapshots'] + filename + misc['ext'],misc['snapshots'] + filename + misc['ext'],pos[image]['x'],pos[image]['y'])
-	background = Image.open(misc['snapshots'] + filename + misc['ext'])
-	foreground = Image.open(misc['cards'] + str(misc['images'][image]) + '.png')
+    resize_canvas(misc['snapshots'] + filename + misc['ext'],misc['snapshots'] + filename + misc['ext'],pos[image]['x'],pos[image]['y'])
+    background = Image.open(misc['snapshots'] + filename + misc['ext'])
+    foreground = Image.open(misc['cards'] + str(misc['images'][image]) + '.png')
 
     print 'merge'
     
