16832b1daa7ad7f1f1fb26b55106fb7d9bc1dcd7
Stephan Kurz <stephan.kurz@mac.com>
2016-03-22 20:18:14 +0100

    plot

--- plot.py
+++ plot.py
@@ -397,14 +397,14 @@ def plot(hashid,image):
     p.print_text(api['protocol'] + api['url'] + '/' + hashid + "\n")
     p.upsidedown_off()
 
-    #from PIL import Image, ImageDraw
-    #i = Image.open(misc['raster'] + str(image) + '.png')
-    #data = list(i.getdata())
-    #w, h = i.size
-    #p.print_bitmap(data, w, h, False)
+    from PIL import Image, ImageDraw
+    i = Image.open(misc['raster'] + str(image) + '.png')
+    data = list(i.getdata())
+    w, h = i.size
+    p.print_bitmap(data, w, h, False)
     
-    p.linefeed()
-    p.linefeed()
+    # p.linefeed()
+    # p.linefeed()
     
 def resize_canvas(old_image_path, new_image_path,
                   x1=0, y1=0,
