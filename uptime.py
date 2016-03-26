d77a1d09c618b11edf858f4d6f919c227d18d8df
Stephan Kurz <stephan.kurz@mac.com>
2016-03-26 19:28:20 +0100

    uptime

--- uptime.py
+++ uptime.py
@@ -3,6 +3,8 @@ from datetime import timedelta
 with open('/proc/uptime', 'r') as f:
     uptime_seconds = float(f.readline().split()[0])
     uptime_string = str(timedelta(seconds = uptime_seconds))
+    
+    print uptime_seconds
             
     message = 'uptime: ' + uptime_string
     print message
