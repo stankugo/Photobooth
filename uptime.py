e9faf72b015d6874e278d57c19525f8213339d1c
Stephan Kurz <stephan.kurz@mac.com>
2016-03-26 19:28:55 +0100

    uptime

--- uptime.py
+++ uptime.py
@@ -4,7 +4,7 @@ with open('/proc/uptime', 'r') as f:
     uptime_seconds = float(f.readline().split()[0])
     uptime_string = str(timedelta(seconds = uptime_seconds))
     
-    print uptime_seconds
+    print int(uptime_seconds)
             
     message = 'uptime: ' + uptime_string
     print message
